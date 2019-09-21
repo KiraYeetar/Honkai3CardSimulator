ff17ae7339283373118a41666e5cead9925f8b45
严武小虎 <424276284@qq.com>
2019-09-21 15:27:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ import random
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 '''
-每次抽卡都会出两件物品，所以当我把两件物品的概率单独计算，模拟概率与实际概率严重不符合... ...
-如果两件物品只计算了一次出货概率
-模拟概率与实际概率拟合...
-讲道理是真滴坑...
+非酋的怒号
 '''
 
 
 def run():
     # 开始模拟翻车抽卡
     simulation_data = []
-    for i in range(10000):
+    for i in range(100000):
         simulation_data += [list(game_begin())]
 
     # 处理下数据
@@ -38,7 +36,7 @@ def run():
     simulation_data = simulation_data
     people_sum = sum(simulation_data['people_count'])
     for i in range(10, 210, 10):
-        print(str(i)+'抽毕业概率: '+str(sum(simulation_data[simulation_data['cards_count']<=i]['people_count'])*1.0/people_sum))
+        print(str(i)+'抽毕业的人数占比: '+str(sum(simulation_data[simulation_data['cards_count']<=i]['people_count'])*1.0/people_sum))
 
     # 开始画图
     simulation_data = simulation_data.sort_values(by='cards_count').set_index('cards_count').head(120)
@@ -54,12 +52,20 @@ def run():
     plt.show()
 
 
-def game_begin(mark_pro=0.0124, weapon_pro=0.02479, other_mark_pro=0.0031*12, other_weapon_pro=0.0062*4):
+'''
+日服概率
+def game_begin(mark_pro=0.0124, weapon_pro=0.02479, other_mark_pro=0.0031*3*4, other_weapon_pro=0.0062*4):
+国服非保底概率
+def game_begin(mark_pro=0.003, weapon_pro=0.0061, other_mark_pro=0.009, other_weapon_pro=0.0061):
+'''
+
+def game_begin(mark_pro=0.003, weapon_pro=0.0061, other_mark_pro=0.009, other_weapon_pro=0.0061):
     mark1_up_pro = mark_pro
     mark2_up_pro = mark1_up_pro + mark_pro
     mark3_up_pro = mark2_up_pro + mark_pro
     weapon_up_pro = mark3_up_pro + weapon_pro
     other_pink_pro = weapon_up_pro+other_mark_pro+other_weapon_pro
+
     is_wish_insure = 0
     is_weapon_insure = 0
     # other_up_pro = 1-mark1_up_pro-mark2_up_pro-mark3_up_pro-weapon_up_pro
@@ -72,38 +78,43 @@ def game_begin(mark_pro=0.0124, weapon_pro=0.02479, other_mark_pro=0.0031*12, ot
     while 0 in god_bless_me:
         # 先扔卡进去
         cards_count += 1
-        # 得出本次概率
+
         one = random.uniform(0, 1)
+        two = random.uniform(0, 1)
+        count_insure_inc = 0
 
         # 十抽保底机制
         if count_insure == 10:
             # print(str(count_all)+'抽，十连保底')
             one = random.uniform(0, other_pink_pro)
 
-        # 武器60抽保底机制
+        # 武器60抽保底机制，覆盖十连保底
         if (cards_count == 60) & (god_bless_me[3] == 0):
             # print('60抽，武器保底')
             one = random.uniform(mark3_up_pro, weapon_up_pro)
             is_weapon_insure = 1
 
-        # 判断出货没有
-        if one < other_pink_pro:
-            count_insure = 0
-            # print(str(count_all) + '抽，出货了')
-            if one < mark1_up_pro:
-                god_bless_me[0] += 1
-                wish_pool[0] = 1
-            elif one < mark2_up_pro:
-                god_bless_me[1] += 1
-                wish_pool[1] = 1
-            elif one < mark3_up_pro:
-                god_bless_me[2] += 1
-                wish_pool[2] = 1
-            elif one < weapon_up_pro:
-                god_bless_me[3] += 1
-        else:
-            # 没出货就保底计数加一
-            count_insure += 1
+        for i in [one, two]:
+            if i < other_pink_pro:
+                count_insure = 0
+                # print(str(count_all) + '抽，出货了')
+                if i < mark1_up_pro:
+                    god_bless_me[0] += 1
+                    wish_pool[0] = 1
+                elif i < mark2_up_pro:
+                    god_bless_me[1] += 1
+                    wish_pool[1] = 1
+                elif i < mark3_up_pro:
+                    god_bless_me[2] += 1
+                    wish_pool[2] = 1
+                elif i < weapon_up_pro:
+                    god_bless_me[3] += 1
+            else:
+                count_insure_inc += 0.5
+
+        # 抽卡结束， 更新多久保底
+        count_insure += int(count_insure_inc)
+        # print(one, two, count_all, god_bless_me)
 
         # 许愿池机制
         if (sum(wish_pool) == 2) & (sum(god_bless_me[0:3]) >= 4):
